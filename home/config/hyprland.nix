5bf20d27a36c18934008ce06201988a2120c393a
realram <mjrodgers05@gmail.com>
2024-08-22 00:16:57 -0600

    build at 2024-08-22 at 00:16

--- home/config/hyprland.nix
+++ home/config/hyprland.nix
@@ -181,11 +181,30 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = "script-nolock -l";
+        lock_cmd = "pidof hyprlock || hyprlock";
         before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
-      listener = [];
+      listener = [
+        {
+          timeout = 20;
+          on-timeout = "brightnessctl -s set 1";
+          on-resume  = "brightnessctl -r";
+        }
+        {
+          timeout = 300; # 5min
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330; # 5.5min
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 1800; # 30min
+          on-timeout = "systemctl hibernate";
+        }
+      ];
     };
   };
 
