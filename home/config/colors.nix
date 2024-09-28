73edb9201cfcf50a20ac897df8c45cdc968403e7
Maxwell Rodgers <mjrodgers05@gmail.com>
2024-09-28 17:21:05 -0600

    build at 2024-09-28 at 17:21

--- home/config/colors.nix
+++ home/config/colors.nix
@@ -3,10 +3,15 @@
 let
   nix-colors-lib = nix-colors.lib.contrib { inherit pkgs; };
 in {
-  colorScheme = nix-colors-lib.nixWallpaperFromScheme {
-      scheme = config.colorScheme;
-      width = 2560;
-      height = 1080;
-      logoScale = 5.0;
+  users = {
+    realram = {
+      colorScheme = nix-colors-lib.nixWallpaperFromScheme {
+        scheme = config.colorScheme;
+        width = 2560;
+        height = 1080;
+        logoScale = 5.0;
+      };
+    };
   };
 }
+
