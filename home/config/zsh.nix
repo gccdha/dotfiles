6ba37570d073310ef43728ca31de63595df9d8a7
Maxwell Rodgers <mjrodgers05@gmail.com>
2024-10-01 10:57:13 -0600

    build at 2024-10-01 at 10:57

--- home/config/zsh.nix
+++ home/config/zsh.nix
@@ -2,7 +2,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
 
