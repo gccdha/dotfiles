145a9c766183211161ab666fba3694e4098ff857
Maxwell Rodgers <mjrodgers05@gmail.com>
2024-10-09 21:47:48 -0600

    build at 2024-10-09 at 21:47

--- config/configuration.nix
+++ config/configuration.nix
@@ -12,48 +12,7 @@
       ./secrets.nix #Time Zone, wg-key
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  boot = {
-    loader = {
-      grub.device = "nodev";
-      grub.efiSupport = true;
-      grub2-theme = {
-        enable = true;
-        theme = "whitesur";
-        footer = true;
-      };
-      efi.canTouchEfiVariables = true;
-    };
-
-    initrd.luks.devices."luks-66f1cdfb-d50f-4982-abfb-8ffe49797927".device = "/dev/disk/by-uuid/66f1cdfb-d50f-4982-abfb-8ffe49797927";
-
-    plymouth = {
-      enable = true;
-      theme = "sphere";
-      themePackages = with pkgs; [
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "sphere" ];
-        })
-      ];
-    };
-
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    initrd.systemd.enable = true;
-
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
-
-  };
-
+  
 
 
   #boot.initrd.luks.devices."luks-66f1cdfb-d50f-4982-abfb-8ffe49797927".device = "/dev/disk/by-uuid/66f1cdfb-d50f-4982-abfb-8ffe49797927";
