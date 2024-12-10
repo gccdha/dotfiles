42020bdb5fad065465a7436c3b3133313cd2523a
Maxwell Rodgers <mjrodgers05@gmail.com>
2024-12-09 18:19:22 -0700

    build at 2024-12-09 at 18:19

---
+++ config/import/uxplay.nix
@@ -0,0 +1,22 @@
+{config, pkgs, ...}:{
+
+  # Open ports
+  networking.firewall.allowedTCPPorts = [7000 7001 7100];
+  networking.firewall.allowedUDPPorts = [5353 6000 6001 7011];
+
+  # enable discovery
+  services.avahi = {
+    enable = true;
+    nssmdns = true; #printing
+    openFirewall = true; #make sure they are open
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+      domain = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [uxplay];
+}
