8531fd29839a6d9c82ef051b8bc4cf8cb8e7e5c9
Maxwell Rodgers <mjrodgers05@gmail.com>
2026-05-08 00:36:15 -0600

    updated due to copy fail

---
+++ battery_notification/battery_notification.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+thresholds=(90 10 5) #battery levels to notify at
+interval=10 #seconds between checking
+
+#create a 1ms dummy notification to get an id we can use for all of the rest
+notification_id=$(notify-send -p "Battery notifications started" --expire-time=1)
+
+pid_flag=0
+num_thresholds=3
+threshold_flags=(0 0 0)
+bat_cap=$(cat /sys/class/power_supply/BAT1/charge_full) # get max chanrge
+
+while sleep "$interval";
+do
+  bat_now=$(cat /sys/class/power_supply/BAT1/charge_now) # get current charge
+  bat_status=$(cat /sys/class/power_supply/BAT1/status) #"Discharging" or "Charging"
+  percent=$(((bat_now*100)/bat_cap))
+
+  #reset threshold_flags if the computer starts charging
+  if [[ $bat_status == "Charging" ]]; then
+    threshold_flags=(0 0 0)
+
+    # if the last notification is still showing, send a notification to indicate charging 
+    kill -0 "$pid" 2>/dev/null
+    if [[ ($? == 0) && ($pid_flag == 1) ]]; then
+      notify-send -r "$notification_id" -t 2000 -i /home/realram/code/misc/bat_icon.svg "Charging" "Battery is charging" -w & 
+      pid_flag=0
+    fi
+  fi
+
+  #check if the notification has been sent yet, send if hasn't and flag as sent
+  if [[ $bat_status == "Discharging" ]]; then
+    for ((i=0;i<num_thresholds;i++));do 
+
+      threshold=${thresholds[$i]}
+      if [[ ( $percent -le ${threshold} ) && ( ${threshold_flags[$i]} == 0 )  ]]; then
+        # Send notification and record the PID
+        notify-send -r "$notification_id" -u critical -i /home/realram/code/misc/bat_icon.svg "BATTERY LOW" "Battery is at $threshold%" -w &
+        pid=$!
+        pid_flag=1
+        threshold_flags[$i]=1
+      fi
+    done
+  fi
+done
+
+
