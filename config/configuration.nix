f3e6c9025aebe80d1e4e455f3a036a21aade8d33
System administrator <mjrodgers05@gmail.com>
2026-05-20 23:59:11 -0600

    patch kernel bluetooth bug

--- config/configuration.nix
+++ config/configuration.nix
@@ -270,4 +270,17 @@
   # 1. python script in home directory
   # 2. added qmk repo in ~/
   # 3. can't figure out why thunar won't detect gvfs
+
+
+
+  boot.kernelPatches = [
+        {
+          name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+          patch = pkgs.fetchurl {
+            url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+            hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+          };
+        }
+      ];
+  warnings = [ "remove kernel patch once issue is resolved: https://github.com/NixOS/nixpkgs/issues/521528" ];
 }
