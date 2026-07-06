aaa246dc833061c816d9d5ee1038aadd72bf002c
System administrator <mjrodgers05@gmail.com>
2026-07-06 12:31:28 -0600

    build at 2026-07-06 at 12:31

--- config/configuration.nix
+++ config/configuration.nix
@@ -273,8 +273,9 @@
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 8080 ];
+
   # networking.firewall.allowedUDPPorts = [ 51820 ];
-  networking.firewall.allowedUDPPorts = [ 8080 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
